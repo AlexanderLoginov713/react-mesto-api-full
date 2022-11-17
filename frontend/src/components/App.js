f199f59aac624de7aa14cc4a843e007f24800edc
AlexanderLoginov713 <95143793+AlexanderLoginov713@users.noreply.github.com>
2022-11-17 22:06:40 +0300

    fix: fix auth

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -110,7 +110,7 @@ function App() {
     api.editProfile(data)
       .then((res) => {
         setCurrentUser(res);
-        console.dir(res);
+        console.dir(res["data"].shift());
         closeAllPopups();
       })
       .catch(err => console.log(`Ошибка: ${err}`))
