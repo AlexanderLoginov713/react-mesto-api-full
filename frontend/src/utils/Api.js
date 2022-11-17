2da1f454a71598f41181d6eeec6a089977b5d125
AlexanderLoginov713 <95143793+AlexanderLoginov713@users.noreply.github.com>
2022-11-18 00:11:20 +0300

    fix: fix auth

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -65,12 +65,22 @@ class Api {
       .then((res) => this._handleResponse(res));
   }
 
-  changeLikeCardStatus(cardId, isLiked) {
-    return fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
-      credentials: 'include',
-      method: `${isLiked ? 'DELETE' : 'PUT'}`,
-      headers: this._headers,
-    }).then((res) => this._handleResponse(res));
+  changeLikeCardStatus(cardId, status) {
+    if (status === true) {
+      return fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
+        method: 'PUT',
+        headers: this._headers,
+        credentials: 'include',
+      })
+        .then(this.handleResponse);
+    } else {
+      return fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
+        method: 'DELETE',
+        headers: this._headers,
+        credentials: 'include',
+      })
+        .then(this.handleResponse);
+    }
   }
 
   deleteIcon(cardId) {
