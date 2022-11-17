b655fcbd55f5633ae44391937f87095c865d0b4d
AlexanderLoginov713 <95143793+AlexanderLoginov713@users.noreply.github.com>
2022-11-18 00:49:01 +0300

    fix: fix auth

--- frontend/src/components/Сard.js
+++ frontend/src/components/Сard.js
@@ -10,7 +10,7 @@ function Card({ cardData, onCardClick, onCardLike, onCardDelete }) {
     `element__trash-btn ${isOwn ? '' : 'element__trash-btn_hidden'}`
   )
   console.dir(cardData);
-  const isLiked = cardData.likes.some((i) => i === currentUser._id);
+  const isLiked = cardData.likes.some((i) => i._id === currentUser._id);
   const cardLikeButtonClassName = (
     `element__like-btn ${isLiked ? 'element__like-btn_active' : ''}`);
 
