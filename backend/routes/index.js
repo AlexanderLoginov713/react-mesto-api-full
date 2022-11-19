87ca8b7c1cf9b2208f7d6eb48a38613a47f7080f
AlexanderLoginov713 <95143793+AlexanderLoginov713@users.noreply.github.com>
2022-11-19 13:38:40 +0300

    fix: fix auth

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -11,11 +11,10 @@ const {
   checkingLogin,
 } = require('../middlewares/validations');
 
-router.use(auth);
 router.post('/logout', logout);
 router.post('/signup', checkingCreateUser, createUser);
 router.post('/signin', checkingLogin, login);
-
+router.use(auth);
 router.use('/users', userRouter);
 router.use('/cards', cardsRouter);
 router.use('*', (req, res, next) => next(new NotFoundError('Запрашиваемый ресурс не найден')));
